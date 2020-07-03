559c5cce9486d46c91dd9281b056338a349bf8f3
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-07-03 09:48:23 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs';
+import * as _ from 'lodash';
 
 import FileSyncAdapter from './adapters/FileSync';
 import DB from './DB';
 
-const fileSyncAdapter = new FileSyncAdapter('src/data-test.json');
+const fileSyncAdapter = new FileSyncAdapter('src/data.json');
 const db = new DB(fileSyncAdapter);
-db.write({});
-const d = db.read();
-console.log({ d });
+const jsonData = db.read();
+
+// console.log({ jsonData });
+// console.log(_.get(jsonData, 'user'));
+
+const sumOfEvenSquares = _.chain(jsonData)
+  .filter((n, i) => console.log({ n, i }))
+  .value();
+
+
+const test = _.chain({ 'test': 1 });
+console.log(test);
+
+// Essentially, we want to set the state into __wrapped__
+// and return DB, that is basically, a lodash chain
