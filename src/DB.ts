b3085f81539eb1c0d43edf392d13c02e57efb557
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-07-03 16:11:20 +0100

    WIP

--- src/DB.ts
+++ src/DB.ts
@@ -1,17 +1,35 @@
-export default class DB {
-  adapter: AdapterInterface;
+import { LoDashStatic } from 'lodash';
 
-  constructor(adapter: AdapterInterface) {
+export default class DB {
+  private lodash: LoDashStatic;
+  private chain: any;
+
+  private adapter: AdapterInterface;
+
+  constructor(lodash: LoDashStatic, adapter: AdapterInterface) {
+    this.lodash = lodash;
+    this.chain = this.lodash.chain({ test: 1 });
     this.adapter = adapter;
+  }
+
+  plant(state: object) {
+    console.log({ state });
+    // eslint-disable-next-line no-underscore-dangle
+    // this.chain.__wrapped__ = state;
+    this.chain.map((n, i) => console.log({ n, i })).value();
+    console.log(this.chain);
+    return this.chain;
   }
 
   write(data: object) {
     this.adapter.write(data);
+
     console.log('Writing some stuff');
     console.log({ data });
   }
 
   read() {
-    return this.adapter.read();
+    const d = this.adapter.read();
+    return this.plant(d);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,18 @@ import FileSyncAdapter from './adapters/FileSync';
 import DB from './DB';
 
 const fileSyncAdapter = new FileSyncAdapter('src/data.json');
-const db = new DB(fileSyncAdapter);
-const jsonData = db.read();
+const db = new DB(_, fileSyncAdapter);
+db.read().filter((n, i) => console.log({ n, i }));
 
-// console.log({ jsonData });
+//console.log({ jsonData });
 // console.log(_.get(jsonData, 'user'));
 
-const sumOfEvenSquares = _.chain(jsonData)
-  .filter((n, i) => console.log({ n, i }))
-  .value();
+// const sumOfEvenSquares = _.chain(jsonData)
+//   .filter((n, i) => console.log({ n, i }))
+//   .value();
 
-
-const test = _.chain({ 'test': 1 });
-console.log(test);
+// const test = _.chain({ 'test': 2 });
+// console.log(test);
 
 // Essentially, we want to set the state into __wrapped__
 // and return DB, that is basically, a lodash chain
